58d4b9144d77573842a2e527160b92b358a5ff92
revathidevik <39332475+revathidevik@users.noreply.github.com>
2020-12-11 23:40:35 +0530

    Add files via upload
    file names model

---
+++ MyAngularAPP/FileNames.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAngularAPP
+{
+    public class FileNames
+    {
+        public List<string> FileName { get; set; }
+    }
+}
