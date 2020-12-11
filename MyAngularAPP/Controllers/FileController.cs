28e1996405ecbbab528530d84d35803708a0909b
revathidevik <39332475+revathidevik@users.noreply.github.com>
2020-12-11 23:39:28 +0530

    Add files via upload
    new API

---
+++ MyAngularAPP/Controllers/FileController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyAngularAPP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetFile")]
+        public ActionResult GetFile()
+        {
+           // FileNames fns = new FileNames();
+            List<string> fn = new List<string>();
+            fn.Add("MyComputer");
+            fn.Add("Videos");
+            fn.Add("Personal");
+            fn.Add("Code");
+
+            return Ok(fn);
+        }
+    }
+}
