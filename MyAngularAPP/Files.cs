b76b866001877f418cf860453097088f3ebed724
revathidevik <39332475+revathidevik@users.noreply.github.com>
2020-12-25 20:21:11 +0530

    Add files via upload
    new form added for model

---
+++ MyAngularAPP/Files.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAngularAPP
+{
+    public class Files
+    {
+        public string Name { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Root
+    {
+        public List<Files> Files { get; set; }
+    }
+}
